70e0cee1f32db288faf5f2a46ebb8d307cd9ac7d
malak sati <97921793+malaksati@users.noreply.github.com>
2022-09-27 21:36:13 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let countDownDate = new Date("Nov 31, 2022 23:59:59").getTime();
+// console.log(countDownDate);
+
+let counter = setInterval(() => {
+
+    // Get Date Now
+    let dateNow = new Date().getTime();
+
+    //Find The Date Difference Between Now And CountDown Date
+    let dateDiff = countDownDate - dateNow;
+
+    // Get Time Units
+    let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+    document.querySelector(".days").innerHTML = days;
+    document.querySelector(".hours").innerHTML = hours;
+    document.querySelector(".minutes").innerHTML = minutes;
+    document.querySelector(".seconds").innerHTML = seconds;
+
+    if (dateDiff < 0) {
+        clearInterval(counter);
+    }
+}, 1000);
